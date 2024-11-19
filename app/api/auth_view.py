8989308bb08218902b2f54ad9809c2afb1461dae
benkeegan98 <ben.keegan98@gmail.com>
2024-11-19 01:38:21 -0800

    some debug logging

--- app/api/auth_view.py
+++ app/api/auth_view.py
@@ -1,11 +1,13 @@
 from flask import redirect, make_response, url_for, request
 from flask.views import MethodView
+import logging
 
 from ..spotify import SpotifyClient
 from ..state import TokenStorage, Auth
 from ..exceptions import MissingRefreshTokenError, MissingTokenDataError, NoAuthenticatedUserError
 from ..utils import TargetEndpoint
 
+logging.basicConfig(level=logging.INFO)
 
 class AuthView(MethodView):
     def dispatch_request(self):
@@ -41,10 +43,16 @@ class AuthView(MethodView):
         if not code:
             return make_response("Authorization failed.", 400)
         
+        logging.info(f"Authorization code received: {code}")
+        
         user = SpotifyClient.get().me()
         Auth.set_user_id(user["id"])
 
+        logging.info(f"Set user id in session: {user["id"]}")
+
         token_data = SpotifyClient.oauth().get_access_token(code, as_dict=True)
+        logging.info(f"Token data: {token_data}")
+
         TokenStorage.set_user_token_data(
             access_token=token_data["access_token"],
             refresh_token=token_data["refresh_token"],
@@ -52,11 +60,13 @@ class AuthView(MethodView):
         )
         
         user_token_data = TokenStorage.get_user_token_data()
+        logging.info(f"Retrieved token data: {user_token_data}")
         if not user_token_data:
             return make_response("Failed to retrieve tokens from TokenStorage.", 400)
 
         # Check if there's a target endpoint stored and redirect there
         if TargetEndpoint.present():
+            logging.info(f"Redirecting to target endpoint")
             return TargetEndpoint.redirect()
         return redirect(url_for('api.auth_success'))
             
